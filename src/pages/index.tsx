f1ca94915fac6304d440ebe42f1b93c030d97f23
Alexandra Tseniklidou <atseniklidou@gmail.com>
2022-01-15 14:18:51 +0200

    new build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { Link } from 'gatsby';
 import * as styles from './Page.module.scss';
 
-const Home = () => 
-    <div className={styles.container}>
+const Home = () => (
+  <div className={styles.container}>
     <h2>Pick one of these</h2>
     <Link className={styles.card} to={'/moving-motivators'}>
       {'Moving Motivators'}
     </Link>
-    </div>;
+  </div>
+);
 
 export default Home;
