e856be881548202ea4d760756465da08e382e94b
Alexandra Tseniklidou <atseniklidou@gmail.com>
2022-02-20 17:27:58 +0200

    analytics for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,25 @@
+import { useEffect } from 'react';
 import ActivityCards from '../components/ActivityCards/ActivityCards';
 import { SEO } from '../components/Seo/Seo';
+import { track } from '../utils/helpers';
 import * as styles from './Page.module.scss';
 
-const NotFoundPage = () => (
-  <>
-    <SEO />
-    <div className={styles.container}>
-      <h1>404 - NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist...the sadness.</p>
-      <h2>Pick one of the activities below</h2>
-      <ActivityCards />
-    </div>
-  </>
-);
+const NotFoundPage = () => {
+  useEffect(() => {
+    track('Hit 404 page');
+  }, []);
+
+  return (
+    <>
+      <SEO />
+      <div className={styles.container}>
+        <h1>404 - NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist...the sadness.</p>
+        <h2>Pick one of the activities below</h2>
+        <ActivityCards />
+      </div>
+    </>
+  );
+};
 
 export default NotFoundPage;
