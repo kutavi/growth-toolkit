d6a5143195beefc353a6d92d18c2f2345f829309
Alexandra Tseniklidou <atseniklidou@gmail.com>
2022-03-07 03:04:29 +0200

    improve form

--- src/components/Subscribe/SubscribeModal.tsx
+++ src/components/Subscribe/SubscribeModal.tsx
@@ -9,7 +9,7 @@ interface SubscribeModalProps {
 }
 
 const iframeSrc =
-  'https://07667286.sibforms.com/serve/MUIEAAXOgVgo7RbLPVoegc8a3g6rC7k8ZJr_HSkFREM_YLWcAxZGEGVcPE4uRIwZ9HMgARs_O5hl0fXDe71NvVq32oMamWmM3yfZpTEec0fEMxc_fSqnyj-BHZbhlLJf_YsqLPm4ZeL515gnXjNjQZRxZDP52mOH8QbK_XJWUJnV6I1e6LHum_GUPgQfMlGyzYj5bOtSQX_S5AKM';
+  'https://07667286.sibforms.com/serve/MUIEALnt-cZuXJTOxnRy38r1d0MrPQeGgNZQ9ii16WXexDVd-pcp6lYE3x7NCW7idBzSzk4QhYJhZlPk-Nvr05g7400mqSYbTAUTOA3IDNu7t8vGvBTGpz3Il4m1nCNtcy08NETqSQFrKr5O66BzlNlGPSRICoWHln2qHsxF4vmZK6rUFahc221GyhudBQKnLY8B3P_fjX4OaDCf';
 
 const SubscribeModal = ({ isOpen, onClose, children }: SubscribeModalProps) => (
   <Modal
