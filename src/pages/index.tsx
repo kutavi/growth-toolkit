65f69b48de8be15cd25ce338fe5fa5736bfde38f
Alexandra Tseniklidou <atseniklidou@gmail.com>
2023-08-24 21:06:06 +0300

    remove subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import ActivityCards from '../components/ActivityCards/ActivityCards';
 import Credits from '../components/Credits/Credits';
 import { SEO } from '../components/Seo/Seo';
-import Subscribe from '../components/Subscribe/Subscribe';
 import * as styles from './Page.module.scss';
 
 const Home = () => (
   <>
     <SEO />
-    <Subscribe />
     <div className={styles.container}>
       <h3>
         A toolkit with exercises and activities to help you on your personal
