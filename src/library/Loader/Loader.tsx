99f4c4e3974d87712f0c02a9e9ef62f9498baa6e
Alex Tseniklidou <atseniklidou@gmail.com>
2025-10-31 21:36:30 +0200

    fix loader

--- src/library/Loader/Loader.tsx
+++ src/library/Loader/Loader.tsx
@@ -1,10 +1,10 @@
 import * as styles from './Loader.module.scss';
 
 export const Loader = () => (
-  <>
+  <div className={styles.loaderContainer}>
     <div className={styles.loaderCircle}>
       <div></div>
     </div>
     <div className={styles.loaderText}>Loading...</div>
-  </>
+  </div>
 );
